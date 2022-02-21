fbbc93d8fcaed7f5885268aee6a52b2af66f58dc
Meena <meenabonam02@gmail.com>
2022-02-21 22:46:26 +0530

    commit

--- Q5.js
+++ Q5.js
@@ -1,14 +1,14 @@
 function btnclicked() 
 {
-  let xhr = new XMLHttpRequest();
-  xhr.open("GET", "https://jsonplaceholder.typicode.com/users", true);
-  xhr.send();
-  xhr.onload = function () {
-    if (xhr.status === 200) 
+  let exp = new XMLHttpRequest();
+  exp.open("GET", "https://jsonplaceholder.typicode.com/users", true);
+  exp.send();
+  exp.onload = function () {
+    if (exp.status === 200) 
     {
       let maindata = JSON.parse(this.response);
       let tds;
-      for (let i =0; i < maindata.length;i++)
+      for (let i =0; i < maindata.length;i++)   
        {
         tds = `
           <tr>
